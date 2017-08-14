246bb41e7343e09b092f8b97897db739a4b63c2d
Stanislaw Szydlo <stanislaw.szydlo@intel.com>
2017-08-14 15:59:20 +0200

    Update to init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-cp .vimrc ~/.vimrc
-cp .tmux.conf ~/.tmux.conf
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+ln -s "$(pwd)"/.zshrc ~/.zshrc
+ln -s "$(pwd)"/.zsh_functions ~/.zsh_functions
+ln -s "$(pwd)"/.vimrc ~/.vimrc
+ln -s "$(pwd)"/.tmux.conf ~/.tmux.conf
 
 # Vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
